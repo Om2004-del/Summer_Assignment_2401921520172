79776d2c91c654917053e8f3f69fb2cb1b1d4940
Om2004-del <odwivedi705@gmail.com>
2026-06-16 22:29:35 +0530

    Update decode-string.cpp

--- Week_2/Day_4/decode-string.cpp
+++ Week_2/Day_4/decode-string.cpp
@@ -1,1 +1,49 @@
+class Solution {
+public:
+    string decodeString(string s) {
 
+        stack<int> counts;
+        stack<string> strings;
+
+        string curr = "";
+        int num = 0;
+
+        for(char ch : s) {
+
+            if(isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if(ch == '[') {
+                counts.push(num);
+                strings.push(curr);
+
+                num = 0;
+                curr = "";
+            }
+
+            else if(ch == ']') {
+
+                int repeat = counts.top();
+                counts.pop();
+
+                string prev = strings.top();
+                strings.pop();
+
+                string temp = "";
+
+                for(int i = 0; i < repeat; i++) {
+                    temp += curr;
+                }
+
+                curr = prev + temp;
+            }
+
+            else {
+                curr += ch;
+            }
+        }
+
+        return curr;
+    }
+};
